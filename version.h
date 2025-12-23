f76121bff419beb2468454b2515c79c0d1003616
jame25 <jame25@users.noreply.github.com>
2025-12-23 01:00:12 +0000

    Update version.h

--- version.h
+++ version.h
@@ -2,9 +2,9 @@
 
 #define FOO_NOWBAR_VERSION_MAJOR 1
 #define FOO_NOWBAR_VERSION_MINOR 0
-#define FOO_NOWBAR_VERSION_PATCH 1
+#define FOO_NOWBAR_VERSION_PATCH 6
 
-#define FOO_NOWBAR_VERSION_STRING "1.0.1"
+#define FOO_NOWBAR_VERSION_STRING "1.0.6"
 #define FOO_NOWBAR_NAME "foo_nowbar"
-#define FOO_NOWBAR_DISPLAY_NAME "Nowbar Control Panel"
+#define FOO_NOWBAR_DISPLAY_NAME "Now Bar Control Panel"
 #define FOO_NOWBAR_DESCRIPTION "Custom control panel with artwork, playback controls, seek bar, and volume."
