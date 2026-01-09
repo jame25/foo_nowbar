686da08b44ce5cc111f6e6ebfe7dfc718a9abbce
jame25 <thefugazi@gmail.com>
2026-01-09 10:40:32 +0000

    Adding Missing SDK files

--- pch.h
+++ pch.h
@@ -34,15 +34,15 @@
 #include <chrono>
 
 // foobar2000 SDK (inside columns_ui) - use full header for all functionality
-#include "../columns_ui/foobar2000/SDK/foobar2000.h"
-#include "../columns_ui/foobar2000/SDK/coreDarkMode.h"
-#include "../columns_ui/foobar2000/helpers/helpers.h"
+#include "lib/columns_ui/foobar2000/SDK/foobar2000.h"
+#include "lib/columns_ui/foobar2000/SDK/coreDarkMode.h"
+#include "lib/columns_ui/foobar2000/helpers/helpers.h"
 
 // Columns UI SDK
-#include "../columns_ui/columns_ui-sdk/ui_extension.h"
+#include "lib/columns_ui/columns_ui-sdk/ui_extension.h"
 
 // SVG Services API (for SVG icon support)
-#include "../columns_ui/svg-services/api/api.h"
+#include "lib/columns_ui/svg-services/api/api.h"
 
 // Project headers
 #include "guids.h"
